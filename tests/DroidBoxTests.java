64d52a51b3e6cb68fb86ac5e787dc9d292035d71
Patrik Lantz <patrik@pjlantz.com>
2011-06-05 12:09:21 +0000

    Added initial test source code and manifest file

---
+++ tests/DroidBoxTests.java
@@ -0,0 +1,14 @@
+package droidbox.tests;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DroidBoxTests extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
